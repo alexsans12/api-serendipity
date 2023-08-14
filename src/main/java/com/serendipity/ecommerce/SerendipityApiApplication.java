2817b403f868f4a99c3c93e572c721f69b91f62b
alexsans12 <eddalexcab@gmail.com>
2023-08-13 21:11:47 -0600

    Init project

---
+++ src/main/java/com/serendipity/ecommerce/SerendipityApiApplication.java
@@ -0,0 +1,13 @@
+package com.serendipity.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerendipityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerendipityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serendipity/ecommerce/SerendipityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serendipity.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerendipityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
